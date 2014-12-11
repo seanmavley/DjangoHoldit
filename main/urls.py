456fe0ee52210ba7c945f84d03944d074f46ac98
Seanmavley <seanmavley@gmail.com>
2014-12-11 02:14:23 +0000

    fixed not serving images

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url, patterns
 from main import views
-from Placeholder import settings
-from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-	url(r'^$', views.home, name='homepage'),
-	url(r'^(?P<values>[d\w]+)/$', views.dimension, name='dimension'),
-	url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/$', views.dimension),
-	#url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/(?P<alpha>[\w]+)/$', views.dimension),	
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^$', views.home, name='homepage'),
+    url(r'^(?P<values>[d\w]+)/$', views.dimension, name='dimension'),
+    url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/$', views.dimension),
+    #url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/(?P<alpha>[\w]+)/$', views.dimension), 
+)
