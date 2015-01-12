3bbdc844d8e20ce1181e796f471dd6bc61228c35
Seanmavley <seanmavley@gmail.com>
2015-01-12 12:22:32 +0000

    fixed not loading of static

--- Placeholder/settings.py
+++ Placeholder/settings.py
@@ -87,6 +87,10 @@ handler404 = 'Menpha.views.file_not_found_404'
 handler500 = 'Menpha.views.server_error'
 handler403 = 'Menpha.views.perm_denied'
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, patterns
 from main import views
+from django.conf.urls.static import static
+from Placeholder import settings
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='homepage'),
@@ -7,4 +9,4 @@ urlpatterns = patterns('',
     url(r'^(?P<values>[d\w]+)/$', views.dimension, name='dimension'),
     url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/$', views.dimension),
     #url(r'^(?P<values>[d\w]+)/as/(?P<color>[d\w].+)/(?P<alpha>[\w]+)/$', views.dimension), 
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
